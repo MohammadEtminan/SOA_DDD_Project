c2a7e55b29533c19a048ea3ce8caea3ce79d37ad
MohammadEtminan <100367714+MohammadEtminan@users.noreply.github.com>
2022-08-29 06:06:35 +0430

    dd

--- OnlineStore.EntityFrameworkCore/Configurations/PersonConfiguration.cs
+++ OnlineStore.EntityFrameworkCore/Configurations/PersonConfiguration.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Domain.Aggregates;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace OnlineStore.EntityFrameworkCore.Configurations
 {
-    internal class PersonConfiguration
+    internal class PersonConfiguration : IEntityTypeConfiguration<Person>
     {
+        #region [- Configure() -] 
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.ToTable("Person", "Person");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.FirstName).IsRequired().HasMaxLength(50);
+            builder.Property(p => p.LastName).IsRequired().HasMaxLength(50);
+
+        }
+        #endregion
     }
 }
---
+++ OnlineStore.EntityFrameworkCore/Frameworks/Base/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnlineStore.Domain.Contract.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.EntityFrameworkCore.Frameworks.Base
+{
+    public class BaseRepository<K_DbContext, T_Entity, U_PrimaryKey> : IRepository<T_Entity, U_PrimaryKey>
+    {
+    }
+}
